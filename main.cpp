eb4a49fdb5911b8891ee7f9fec33e823e8c4959b
Jonathan Jones <jonesjon@flip1.engr.oregonstate.edu>
2018-04-17 12:58:29 -0700

    New Branch: Added initial cpp file and .gitignore
    main.cpp validates number of command line arguments and .gitignore ignores a.out files.

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+// <initial state file> <goal state file> <mode> <output file>
+int main(int argc, char* argv[])
+{
+    //Initialize command line argument buffers
+    string state_file, goal_state_file, mode, output_file;
+    
+    //Check for valid number of arguments
+    if(argc == 5)
+        state_file = argv[1], goal_state_file = argv[2], mode = argv[3], output_file = argv[4];
+    else 
+    {
+        cout << "USAGE: Invalid argument count" << endl;
+        return 1;
+    }
+
+    return 0;
+}
