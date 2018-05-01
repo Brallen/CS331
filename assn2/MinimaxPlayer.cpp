9cd454de00fe4a7696b4e94810d25566ec807e3b
Brallen <brett.case97@gmail.com>
2018-05-01 12:05:58 -0700

    added algorithm outline

--- assn2/MinimaxPlayer.cpp
+++ assn2/MinimaxPlayer.cpp
@@ -18,7 +18,23 @@ MinimaxPlayer::MinimaxPlayer(char symb) :
 MinimaxPlayer::~MinimaxPlayer() {
 
 }
-
+// function MINIMAX -DECISION(state) returns an action
+// 	inputs: state, current state in game
+// 	v← MAX-VALUE(state)
+// 	return the action in SUCCESSORS(state) with value v
+// 
+// function MAX-VALUE(state) returns a utility value
+// 	if TERMINAL-TEST(state) then return UTILITY(state)
+// 	v← -Infinity
+// 	for a, s in SUCCESSORS(state) do 
+// 		v← MAX(v, MIN-VALUE(s))
+// 	return v
+// 
+// function MIN-VALUE(state) returns a utility value
+// 	if TERMINAL-TEST(state) then return UTILITY(state)v ← Infinity
+// 	for a, sin SUCCESSORS(state) do
+// 		v← MIN(v, MAX-VALUE(s))
+// 		return v
 void MinimaxPlayer::get_move(OthelloBoard* b, int& col, int& row) {
     // To be filled in by you
 }
